590ca808a02a4f4b3ee40a5e3997f6eb46b66c2f
WameJoseph <101824766+WameJoseph@users.noreply.github.com>
2022-09-15 11:53:45 +0100

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,11 @@
+function makeNavAppear() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("sidebar").style.height = "100%";
+    document.getElementById("sidebar").style.visibility = "visible";
+
+}
+function makeNavDissapear() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").style.height = "100%";
+    document.getElementById("sidebar").style.visibility = "hidden";
+}
