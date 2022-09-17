67291595ed21de19a305912dc55a60d28f10d079
WameJoseph <101824766+WameJoseph@users.noreply.github.com>
2022-09-17 14:06:58 +0100

    Add files via upload

--- mainpage.js
+++ mainpage.js
@@ -1,3 +1,34 @@
+// function makeSearchXDisplay(){
+//     if (window.matchMedia("(max-width: 850px)")).matches{     
+//     document.getElementById("cancelsearch").style.visibility ="visible";
+//     document.getElementById("search").style.outline ="solid";
+//     document.getElementById("search").style.width ="90%";
+//     document.getElementById("search").style.position ="absolute";
+//     document.getElementById("search").style.backgroundColor ="white";
+//     document.getElementById("search").style.marginLeft ="0";
+//     document.getElementById("search").style.marginRight ="auto";
+//     document.getElementById("searchinput").style.width ="80%";
+//     document.body.backgroundColor = "blue";}
+    
+//     else{
+//         document.body.backgroundColor = "orange";}
+//     }
+
+// }
+
+// function makeSearchXnon(){
+//     document.getElementById("cancelsearch").style.visibility ="hidden";
+//     document.getElementById("search").style.outline ="none";
+//     document.getElementById("search").style.width ="45%";
+//     document.getElementById("search").style.position ="static";
+//     document.getElementById("search").style.backgroundColor ="rgba(226, 226, 226, 0.6)";
+//     document.getElementById("search").style.marginLeft ="0";
+//     document.getElementById("search").style.marginRight ="10px";
+//     document.getElementById("searchinput").style.width ="45%";
+//     document.body.backgroundColor = "blue";
+// }
+    
+
 function makeNavAppear() {
     document.getElementById("sidebar").style.width = "300px";
     document.getElementById("sidebar").style.height = "100%";
@@ -9,3 +40,6 @@ function makeNavDissapear() {
     document.getElementById("sidebar").style.height = "100%";
     document.getElementById("sidebar").style.visibility = "hidden";
 }
+
+
+
